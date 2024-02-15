9e51cbe412f7643d12667a42957afe709845e927
Deiber Cardona <deiberacd@gmail.com>
2024-02-15 04:11:50 -0500

    se agrega funion orden individual y se estiliza ordenes de compra

--- src/Components/OrdersCard/index.jsx
+++ src/Components/OrdersCard/index.jsx
@@ -1,16 +1,33 @@
-import { XMarkIcon } from '@heroicons/react/24/solid'
+import { ChevronRightIcon } from '@heroicons/react/24/solid'
+import { CalendarDaysIcon } from '@heroicons/react/24/solid'
+import { ShoppingBagIcon } from '@heroicons/react/24/solid'
 
 const OrdersCard = props => {
   const { totalPrice, totalProducts } = props
  
 
   return (
-    <div className="flex justify-between items-center mb-3 border-black">
-      <p>
-        <span>01.02.2023</span>
-        <span>{ totalProducts }</span>
-        <span>{ totalPrice }</span>
-      </p>
+    <div className="flex justify-between items-center bg-gray-300 mb-3 border border-black rounded-lg p-4 w-80 ">
+      <div className='flex flex-col justify-between w-full'>
+        
+        <p className='flex  items-center gap-2'>
+        <CalendarDaysIcon className='flex h-6 w-6 text-black cursor-pointer'></CalendarDaysIcon>
+        <span className='font-light'>01.02.2023</span>
+        </p>
+
+        <p className='flex  items-center gap-2'>
+        <ShoppingBagIcon className='flex h-6 w-6 text-black cursor-pointer'></ShoppingBagIcon>
+        <span className='font-light'>{ totalProducts } articles</span>
+        </p>
+
+      </div>
+
+        <div>
+          <p className='flex items-center gap-2'>
+          <span className='font-medium text-2xl'> ${ totalPrice }</span>
+          <ChevronRightIcon className='h-6 w-6 text-black cursor-pointer'></ChevronRightIcon>
+          </p>
+        </div>
     </div>
   )
 }
